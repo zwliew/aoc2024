e82ccde14deb6502625e2e34e3e183b4a193f054
Zhao Wei Liew <zhaoweiliew@gmail.com>
2024-12-07 09:42:35 +0800

    Add day3

--- app/Day3.hs
+++ app/Day3.hs
@@ -1,9 +1,57 @@
 module Day3 where
 
+import Control.Applicative
+import Text.Parsec (anyChar, digit, many1, parse, string, try)
+import Text.Parsec.String (Parser)
+
+data Match = Do | Dont | Mul (Int, Int) | Skipped
+    deriving (Show, Eq)
+
+parseDo :: Parser Match
+parseDo = Do <$ string "do()"
+
+parseDont :: Parser Match
+parseDont = Dont <$ string "don't()"
+
+parseMul :: Parser Match
+parseMul = do
+    _ <- string "mul("
+    x <- many1 digit
+    _ <- string ","
+    y <- many1 digit
+    _ <- string ")"
+    return $ Mul (read x, read y)
+
+skipOneChar :: Parser Match
+skipOneChar = anyChar >> return Skipped
+
+parsePattern :: Parser Match
+parsePattern = try parseDo <|> try parseDont <|> try parseMul <|> skipOneChar
+
+parseInput :: String -> Parser a -> a
+parseInput input parser = case parse parser "" input of
+    Left err -> error $ show err
+    Right res -> res
+
+walkTokens :: (Bool -> Bool) -> (Bool, Int) -> Match -> (Bool, Int)
+walkTokens _ (b, n) Skipped = (b, n)
+walkTokens _ (_, n) Do = (True, n)
+walkTokens _ (_, n) Dont = (False, n)
+walkTokens f (b, n) (Mul (x, y)) = (b, n + if f b then x * y else 0)
+
+getInput :: IO [Match]
+getInput = do
+    input <- readFile "inputs/day3.txt"
+    return $ parseInput input (many1 parsePattern)
+
 solve1 :: IO String
 solve1 = do
-    return ""
+    matches <- getInput
+    let multSum = snd $ foldl (walkTokens (const True)) (True, 0) $ filter (/= Skipped) matches
+    return $ show multSum
 
 solve2 :: IO String
 solve2 = do
-    return ""
+    matches <- getInput
+    let multSum = snd $ foldl (walkTokens id) (True, 0) $ filter (/= Skipped) matches
+    return $ show multSum
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Day2 (solve1, solve2)
+import Day3 (solve1, solve2)
 
 main :: IO ()
 main = solve1 >>= putStrLn >> solve2 >>= putStrLn
